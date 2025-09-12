2f7210f55840356185ec32bac367593bfc9da333
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-09-12 12:14:29 +0900

    0.1.6

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
-  let { children, onclick} = $props()
+  import type { Snippet } from "svelte";
+
+  interface Button {
+    children: Snippet
+    onclick?: (e: Event) => void
+    href?: string
+  }
+
+  let { children, onclick, href }: Button = $props();
 </script>
 
-<button {onclick} class="btn btn-success p-3 font-bold">{@render children()}</button>
+{#if href}
+  <a {href} class="btn btn-lg btn-circle p-3 font-bold" target="_blank">{@render children()}</a>
+{:else}
+  <button {onclick} class="btn btn-success p-3 font-bold"
+    >{@render children()}</button
+  >
+{/if}
