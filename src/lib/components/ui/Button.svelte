81f88bb4b9ba01dbdd17cf9090db63821dfd2b42
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-09-17 05:42:51 +0900

    1.2.12

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -2,10 +2,10 @@
   import type { Snippet } from "svelte";
 
   interface Button {
-    children: Snippet
-    onclick?: (e: Event) => void
-    href?: string
-    disabled?: boolean
+    children: Snippet;
+    onclick?: (e: Event) => void;
+    href?: string;
+    disabled?: boolean;
   }
 
   let { children, onclick, href, disabled = false }: Button = $props();
@@ -14,7 +14,7 @@
 {#if href}
   <a {href} class="btn btn-lg btn-circle p-3 font-bold">{@render children()}</a>
 {:else}
-  <button {onclick} {disabled} class="btn btn-success p-3 font-bold" 
+  <button {onclick} {disabled} class="btn btn-success p-3 font-bold"
     >{@render children()}</button
   >
 {/if}
